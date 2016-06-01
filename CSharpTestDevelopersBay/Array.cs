62e254854055c601615120bffff4196baadb40be
Jim Aho <jiim.aho@gmail.com>
2016-06-01 16:22:47 +0200

    Initial commit

---
+++ CSharpTestDevelopersBay/Array.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTestDevelopersBay
+{
+    using System;
+
+    public static class Vectors
+    {
+        public static int[] FindShortest(int[][] vectors)
+        {
+            var numberOfVectors = vectors.Count();
+            var lengtPerVector = new List<KeyValuePair<int, int>>(numberOfVectors);
+
+            for(int i = 0; i < vectors.Count(); i++)
+            {
+                var vector = vectors[i];
+                var length = LengthOfVector(vector);
+                lengtPerVector.Add(new KeyValuePair<int, int>(i, length));
+                
+            }
+
+            return null;
+        }
+
+        public static int LengthOfVector(int[] vector)
+        {
+            if(vector == null || vector.Count() != 3)
+            {
+                throw new Exception("Invalid vector");
+            }
+
+            var x = vector[0];
+            var y = vector[1];
+            var z = vector[2];
+
+            var xPow = Math.Pow(x, 2);
+            var yPow = Math.Pow(y, 2);
+            var zPow = Math.Pow(z, 2);
+
+            return (int) Math.Sqrt(xPow + yPow + zPow);
+        }
+
+        public static void Main(string[] args)
+        {
+            int[][] vectors = 
+            {
+                new int[] { 1, 1, 1 },
+                new int[] { 2, 2, 2 },
+                new int[] { 3, 3, 3 }
+            };
+
+            int[] shortest = Vectors.FindShortest(vectors);
+            // Expected output: x: 1, y: 1, z: 1
+            Console.WriteLine(String.Format("x: {0}, y: {1}, z: {2}", shortest[0], shortest[1], shortest[2]));
+        }
+    }
+}
---
+++ CSharpTestDevelopersBay/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTestDevelopersBay
+{
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+
+    //    }
+    //}
+
+    using System;
+
+    public enum Side { None, Left, Right }
+
+    public class Link
+    {
+        public Link Left { get; private set; }
+        public Link Right { get; private set; }
+
+        public void Append(Link rightPart)
+        {
+            if (this.Right != null)
+                throw new InvalidOperationException("Link is already connected.");
+
+            this.Right = rightPart;
+            rightPart.Left = this;
+        }
+    }
+
+    public class Chain
+    {
+        public static Side WhichSideIsLonger(Link link)
+        {
+            if(link.Left == null && link.Right == null)
+            {
+                return Side.None;
+            }
+
+            if (NumberAtRight(link) > NumberAtLeft(link))
+            {
+                return Side.Right;
+            }
+
+            return Side.Left;
+        }
+
+        public static int NumberAtRight(Link link)
+        {
+            if (link == null || link.Right == null)
+            {
+                return 0;
+            }
+
+            return 1 + NumberAtRight(link.Right);
+        }
+
+        public static int NumberAtLeft(Link link)
+        {
+            if (link == null || link.Left == null)
+            {
+                return 0;
+            }
+
+            return 1 + NumberAtLeft(link.Left);
+        }
+
+        //public static void Main(string[] args)
+        //{
+        //    Link left = new Link();
+        //    Link middle = new Link();
+        //    Link right = new Link();
+        //    left.Append(middle);
+        //    middle.Append(right);
+        //    Console.WriteLine(Chain.WhichSideIsLonger(left));
+        //}
+    }
+}
